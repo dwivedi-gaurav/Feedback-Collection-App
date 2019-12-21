0df34fad50c03f8bda784ebb23e74469171f659e
Gaurav Dwivedi <dwivedig@HCL.COM>
2019-12-21 20:40:16 +0530

    Greeting changed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express=require('express');
 const app=express();
 
 app.get('/',(req,res)=>{
-    res.send({hi:"Welcome to fullStack Development..."});
+    res.send({Hey:"Welcome to fullStack Development..."});
 })
 
 const PORT=process.env.PORT || 5000;
